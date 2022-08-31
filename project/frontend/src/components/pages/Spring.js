42cf5a672776105198b7eb41d89fd223bfd4e9df
Giovani Sanches <giovanisan6@gmail.com>
2022-08-31 20:02:01 -0300

    Project files

---
+++ project/frontend/src/components/pages/Spring.js
@@ -0,0 +1,13 @@
+import styles from './Spring.module.css'
+
+function Spring() {
+    return (
+        <section className={styles.spring_container}>
+         <button>
+                Fazer requisição Spring
+        </button>
+        </section>
+    )
+}
+
+export default Spring
---
+++ project/frontend/src/components/pages/Symfony.js
@@ -0,0 +1,13 @@
+import styles from './Symfony.module.css'
+
+function Symfony() {
+    return (
+        <section className={styles.symfony_container}>
+         <button>
+                Fazer requisição Symfony
+        </button>
+        </section>
+    )
+}
+
+export default Symfony
