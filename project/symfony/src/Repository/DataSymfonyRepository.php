6d680659f28483d64825c119fc5c8b0427db9b71
gamsanches <60519121+gamsanches06@users.noreply.github.com>
2022-11-06 18:38:36 -0300

    Update DataSymfonyRepository.php

--- project/symfony/src/Repository/DataSymfonyRepository.php
+++ project/symfony/src/Repository/DataSymfonyRepository.php
@@ -38,29 +38,4 @@ class DataSymfonyRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return DataSymfonyController[] Returns an array of DataSymfonyController objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('d.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?DataSymfonyController
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
