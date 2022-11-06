d88aec7ae44420c619a8f5699a73d438016b8946
gamsanches <60519121+gamsanches06@users.noreply.github.com>
2022-11-06 19:01:43 -0300

    Update Sobre.js

--- project/frontend/src/components/pages/Sobre.js
+++ project/frontend/src/components/pages/Sobre.js
@@ -8,13 +8,13 @@ function Sobre() {
 
             <h1><span>Sobre nós</span></h1>
             <p>Somos um grupo de três estudantes de Ciências da Computação, na Universidade Paulista - UNIP.
-                Apaixonados pela tecnologia e suas capacidades, decidimos criar esse projeto de conclusão de curso no intuito de desmontrar
+                Apaixonados pela tecnologia e suas capacidades, decidimos criar esse projeto de conclusão de curso no intuito de demonstrar
                 o poder de cada framework escolhido, comparando sua performance, implementação, custo, manutenção etc.
                 Ao final deste trabalho, conseguiremos ter uma visão geral da melhor escolha para determinada finalidade, seja para uma empresa,
                 projeto pessoal ou afins de estudo.</p>
 
             <div className={styles.imagem}>
-            <img src={sobre} alt="Sobre nos" />
+            <img src={sobre} alt="Sobre nós" />
             <p>Da esquerda pra direita: Giovani Sanches, Yago Motti e Felipe Milani</p>
             </div>
         </section>
