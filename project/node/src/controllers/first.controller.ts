715c6f0cd600756d29b7ea65938ff8b9a296fd4f
yagomotti <yagomotti@gmail.com>
2022-11-13 18:38:00 -0300

    node insert

--- project/node/src/controllers/first.controller.ts
+++ project/node/src/controllers/first.controller.ts
@@ -1,49 +1,58 @@
 import { Request, Response } from "express";
 const { performance } = require('perf_hooks');
 import data from "./data.json";
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: "rootsecret",
-  database: 'tcc_2022'
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: '3306',
+//   user: 'root',
+//   password: "ronalduchi12",
+//   database: 'tcc_2022'
+// });
 
-function selectData() {
-  connection.query(
-    'SELECT * FROM `data_node`',
-    function (err: any, results: any, fields: any) {
-      return results;
-    }
-  );
+async function connect() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: "ronalduchi12",
+    database: 'tcc_2022'
+  });
+  return connection;
 }
 
-function insertData(parsed: any) {
-  parsed.forEach((data: any) => {
+async function insertData(parsed: any, qtd: number) {
+  const conn = await connect();
 
-  connection.query(`INSERT INTO data_node (isActive, balance, age, name, gender, company, email, phone, address) VALUES 
-  (?,?,?,?,?,?,?,?,?)`, [data.isActive, data.balance, data.age, data.name, data.gender, data.company, data.email, data.phone, data.address],
-    (error: any, results: any) => {
-      if (error) console.log(error); ({ error: error });
-    });
-  });
-  
-  return true;
+  for (let i = 0; i < qtd; i++) {
+    const data = parsed[i];
+
+    await conn.query(`INSERT INTO data_node (isActive, balance, age, name, gender, company, email, phone, address) VALUES 
+    (?,?,?,?,?,?,?,?,?)`, [data.isActive, data.balance, data.age, data.name, data.gender, data.company, data.email, data.phone, data.address],
+      (error: any, results: any) => {
+        if (error) console.log(error); ({ error: error });
+      });
+  }
+
+  const [rows] = await conn.query('SELECT COUNT(name) FROM data_node;');
+  return rows;
+
 }
 
 class FirstController {
-  public home(req: Request, res: Response) {
+  public async home(req: Request, res: Response) {
     var startTime = performance.now()
 
     const parsed = data as unknown[];
-     insertData(parsed);
-  
+    const qtd = 5000;
+    // const qtd = req.query.qtd || 0;
+
+    await insertData(parsed, qtd);
 
     var endTime = performance.now()
     return res.json({
-      response: `Call to insert data took ${endTime - startTime} milliseconds.`
+      response: `Call to insert ${qtd} took ${endTime - startTime} milliseconds.`
     });
   }
 }
